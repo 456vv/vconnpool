ff1aab5f0e1b7bc89940c61d660ab07f0ff188a1
456vv <123@456vv.com>
2016-08-17 10:24:10 +0800

    Add files via upload

---
+++ connPool_test.go
@@ -0,0 +1,236 @@
+package vconnpool
+
+import (
+	"testing"
+    "time"
+    "net"
+)
+
+
+func Test_ConnPool_1(t *testing.T){
+return
+    cp := &ConnPool{
+        IdeConn:5,
+        MaxConn:2,
+    }
+    defer cp.Close()
+    netConn1, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+
+    netConn2, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+    netConn1.Close()
+
+    netConn3, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+    netConn2.Close()
+    netConn3.Close()
+
+    key := connAddr{"tcp", "www.baidu.com:80"}
+    l := len(cp.conns[key])
+    if l != 2 {
+        t.Fatalf("错误：无法加入空闲连接到池中，池中连接数量：%d", l)
+    }
+}
+func Test_ConnPool_2(t *testing.T){
+return
+    cp := &ConnPool{
+        Dialer:net.Dialer{},
+        IdeConn:5,
+    }
+    defer cp.Close()
+    cp.Timeout = 1*time.Second
+    cp.KeepAlive = 1*time.Second
+    netConn4, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+    netConn4.Close()
+
+
+}
+
+func Test_ConnPool_3(t *testing.T){
+return
+    cp := &ConnPool{
+        Dialer:net.Dialer{},
+        IdeConn:5,
+    }
+    defer cp.Close()
+    cp.Timeout = 1*time.Minute
+    cp.KeepAlive = 1*time.Minute
+    netConn1, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+    netConn1.Write([]byte("GET / HTTP/1.1\r\nHost:www.baidu.com\r\nConnection:Close\r\n\r\n"))
+    p := make([]byte, 1024)
+    n, err := netConn1.Read(p)
+    if err != nil {
+        t.Fatal(err)
+    }
+    t.Log(n)
+
+    netConn1.Close()
+
+    time.Sleep(time.Second)
+
+    netConn2, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {
+    	t.Fatal(err)
+    }
+    netConn2.Write([]byte("GET / HTTP/1.1\r\nHost:www.baidu.com\r\nConnection:Close\r\n\r\n"))
+    p = make([]byte, 1024)
+    n, err = netConn2.Read(p)
+    if err != nil {
+        t.Fatal(err)
+    }
+    t.Log(n)
+
+    netConn2.Close()
+
+}
+
+func Test_ConnPool_4(t *testing.T){
+    cp := &ConnPool{
+        Dialer:net.Dialer{},
+        IdeConn:5,
+    }
+    defer cp.Close()
+    cp.Timeout = 1*time.Minute
+    cp.KeepAlive = 1*time.Minute
+
+    netConn1, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    netConn1.Write([]byte("GET / HTTP/1.1\r\nHost:www.baidu.com\r\nConnection:Close\r\n\r\n"))
+    p := make([]byte, 1024)
+    n, err := netConn1.Read(p)
+    if err != nil {t.Fatal(err)}
+    t.Log(n)
+    netConn1.Close()
+
+    time.Sleep(time.Second)
+    if cp.ConnNum() != 1 {
+        t.Fatalf("netConn1:池里的连接数量不符，返回为：%d，预设为：1", cp.ConnNum())
+    }
+
+    netConn2, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    netConn2.Write([]byte("GET / HTTP/1.1\r\nHost:www.baidu.com\r\nConnection:Close\r\n\r\n"))
+    p = make([]byte, 1024)
+    n, err = netConn2.Read(p)
+    if err != nil {t.Fatal(err)}
+    t.Log(n)
+    netConn2.Close()
+
+    time.Sleep(time.Second)
+    if cp.ConnNum() != 1 {
+        t.Fatalf("netConn2:池里的连接数量不符，返回为：%d，预设为：1", cp.ConnNum())
+    }
+
+    netConn3, err := net.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    defer netConn3.Close()
+    err = cp.Put(netConn3)
+    if err != nil {t.Fatal(err)}
+    if cp.ConnNum() != 2 {
+        t.Fatalf("netConn3:池里的‘可用’连接数量不符，返回为：%d，预设为：2", cp.ConnNum())
+    }
+
+    netConn4, err := net.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    defer netConn4.Close()
+    err = cp.Put(netConn4)
+    if err != nil {t.Fatal(err)}
+    if cp.ConnNum() != 3 {
+        t.Fatalf("netConn4:池里的连接数量不符，返回为：%d，预设为：3", cp.ConnNum())
+    }
+
+    _, err = cp.Get(netConn3.RemoteAddr())
+    if err != nil {t.Fatal(err)}
+    if cp.ConnNum() != 2 {
+        t.Fatalf("Get:池里的连接数量不符，返回为：%d，预设为：2", cp.ConnNum())
+    }
+
+    _, err = cp.Get(netConn4.RemoteAddr())
+    if err != nil {t.Fatal(err)}
+    if cp.ConnNum() != 1 {
+        t.Fatalf("Get:池里的连接数量不符，返回为：%d，预设为：1", cp.ConnNum())
+    }
+
+    cp.Close()
+    cp.Close()
+    cp.CloseIdleConnections()
+    cp.CloseIdleConnections()
+    cp.Close()
+    cp.CloseIdleConnections()
+    if cp.ConnNum() != 0 {
+        t.Fatalf("Get:池里的连接数量不符，返回为：%d，预设为：0", cp.ConnNum())
+    }
+
+}
+
+
+func Test_ConnPool_5(t *testing.T){
+return
+    cp := &ConnPool{
+        IdeConn:5,
+        MaxConn:2,
+    }
+    defer cp.Close()
+    conn, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    conn.Close()
+
+    conn, err = cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    c, ok := conn.(Conn)
+    if !ok {
+        t.Fatal("不支持转换为 Conn 接口")
+    }
+    if cp.ConnNum() != 1 {
+        t.Fatalf("池里的连接数量不符，返回为：%d，预设为：1", cp.ConnNum())
+    }
+    c.Discard()
+    c.Close()
+    if cp.ConnNum() != 0 {
+        t.Fatalf("池里的连接数量不符，返回为：%d，预设为：0", cp.ConnNum())
+    }
+}
+
+func Test_ConnPool_6(t *testing.T){
+ return
+   cp := &ConnPool{
+        IdeConn:5,
+        MaxConn:2,
+    }
+    defer cp.Close()
+    conn, err := cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    conn.Close()
+
+    conn, err = cp.Dial("tcp", "www.baidu.com:80")
+    if err != nil {t.Fatal(err)}
+    if cp.ConnNum() != 1 {
+        t.Fatalf("池里的连接数量不符，返回为：%d，预设为：1", cp.ConnNum())
+    }
+    conn.Close()
+
+    ideCount := cp.ConnNumIde("tcp", "www.baidu.com:80")
+    if ideCount != cp.ConnNum() {
+        t.Fatal("空闲连接和可用连接数量不符，空闲为：%s，可用为：%s", ideCount, cp.ConnNum())
+    }
+    cp.Close()
+    //池被清空，空闲和实用连接都为0
+    ideCount = cp.ConnNumIde("tcp", "www.baidu.com:80")
+    if ideCount != 0 || cp.ConnNum() !=0 {
+        t.Fatal("空闲连接和可用连接数量不符，空闲为：%s，可用为：%s", ideCount, cp.ConnNum())
+    }
+
+}

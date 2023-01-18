eb42b9cd8371c9ba1da9042c3027f91f902ae1ef
456vv <123@456vv.com>
2023-01-18 17:25:50 +0800

    将net.Dialer 改为 Dialer 接口

--- connPool_test.go
+++ connPool_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/456vv/vconn"
 	"github.com/456vv/x/tcptest"
 
-	//"github.com/alecthomas/assert"
 	"github.com/issue9/assert/v2"
 )
 
@@ -17,10 +16,8 @@ func Test_ConnPool_1(t *testing.T) {
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		defer c.Close()
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
 			IdeConn: 5,
@@ -28,12 +25,12 @@ func Test_ConnPool_1(t *testing.T) {
 		}
 		defer cp.Close()
 
-		//创建连接
+		// 创建连接
 		conn1, err := cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
 		conn2, err := cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
-
+		// 回收到池里
 		conn1.Close()
 		conn2.Close()
 
@@ -55,23 +52,20 @@ func Test_ConnPool_1(t *testing.T) {
 	})
 }
 
-//检查池中的数量
+// 检查池中的数量
 func Test_ConnPool_2(t *testing.T) {
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
-			Dialer:  &net.Dialer{},
 			IdeConn: 5,
 		}
 		defer cp.Close()
 
-		//创建连接
+		// 创建连接
 		conn, err := cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
 		conn.Close()
@@ -83,26 +77,22 @@ func Test_ConnPool_2(t *testing.T) {
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 1)
 	})
-
 }
 
-//读取原始连接，并关闭
+// 读取原始连接，并关闭
 func Test_ConnPool_4(t *testing.T) {
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
-			Dialer:  &net.Dialer{},
 			IdeConn: 5,
 		}
 		defer cp.Close()
 
-		//创建连接
+		// 创建连接
 		conn, err := cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
 		conn.Close()
@@ -114,15 +104,15 @@ func Test_ConnPool_4(t *testing.T) {
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 1)
 
-		//从池中读出
+		// 从池中读出
 		conn, err = cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
 
-		//原始连接
+		// 原始连接
 		netConn_2 := conn.(Conn).RawConn()
 		netConn_2.Close()
 
-		//没有用处，因为已经使用RawConn读取连接
+		// 不会被回收，因为已经使用RawConn读取连接
 		conn.Close()
 
 		d = cp.ConnNum()
@@ -134,29 +124,25 @@ func Test_ConnPool_4(t *testing.T) {
 	})
 }
 
-//使用GET读取连接，池中的当前连接数量有变化
+// 使用GET读取连接，池中的当前连接数量有变化
 func Test_ConnPool_5(t *testing.T) {
-
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
-			Dialer:  &net.Dialer{},
 			IdeConn: 5,
 		}
 		defer cp.Close()
 
-		//创建连接
+		// 创建连接
 		conn, err := cp.Dial(raddr.Network(), raddr.String())
 		as.NotError(err)
 		defer conn.Close()
 
-		//加入池中
+		// 加入池中
 		err = cp.Add(conn)
 		as.NotError(err)
 
@@ -167,7 +153,7 @@ func Test_ConnPool_5(t *testing.T) {
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 1)
 
-		//从池中读取
+		// 从池中读取
 		tconn, err := cp.Get(conn.RemoteAddr())
 		as.NotError(err)
 		tconn.Close()
@@ -187,16 +173,13 @@ func Test_ConnPool_5(t *testing.T) {
 	})
 }
 
-//废弃连接，不入池
+// 废弃连接，不入池
 func Test_ConnPool_6(t *testing.T) {
-
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
 			IdeConn: 5,
@@ -221,7 +204,7 @@ func Test_ConnPool_6(t *testing.T) {
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 0)
 
-		//废弃这个连接，不让他进入池内
+		// 废弃这个连接，不让他进入池内
 		c1.Discard()
 		c1.Close()
 
@@ -230,15 +213,13 @@ func Test_ConnPool_6(t *testing.T) {
 	})
 }
 
-//检查连接数量和空闲数量
+// 检查连接数量和空闲数量
 func Test_ConnPool_7(t *testing.T) {
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
 			IdeConn: 5,
@@ -255,10 +236,10 @@ func Test_ConnPool_7(t *testing.T) {
 
 		time.Sleep(5 * time.Millisecond)
 
-		//正在使用的连接数量
+		// 正在使用的连接数量
 		as.Equal(cp.ConnNum(), 1)
 
-		//池中的连接数量
+		// 池中的连接数量
 		d := cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 0)
 
@@ -268,18 +249,15 @@ func Test_ConnPool_7(t *testing.T) {
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
 		as.Equal(d, 1)
 	})
-
 }
 
-//判断连接池中的数量是否正确
+// 判断连接池中的数量是否正确
 func Test_ConnPool_8(t *testing.T) {
 	as := assert.New(t, true)
 
 	tcptest.D2S("127.0.0.1:0", func(c net.Conn) {
-		select {
-		case <-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify():
-			c.Close()
-		}
+		<-vconn.NewConn(c).(vconn.CloseNotifier).CloseNotify()
+		c.Close()
 	}, func(raddr net.Addr) {
 		cp := &ConnPool{
 			IdeConn: 5,
@@ -291,7 +269,7 @@ func Test_ConnPool_8(t *testing.T) {
 		as.NotError(err)
 		conn.Close()
 
-		ctx := context.WithValue(context.Background(), "priority", true) //新创建连接
+		ctx := context.WithValue(context.Background(), PriorityContextKey, true) // 新创建连接
 		conn, err = cp.DialContext(ctx, raddr.Network(), raddr.String())
 		as.NotError(err)
 		conn.Close()
@@ -301,7 +279,7 @@ func Test_ConnPool_8(t *testing.T) {
 		as.Equal(d, 2)
 
 		cp.CloseIdleConnections()
-		time.Sleep(time.Second)
+		time.Sleep(time.Millisecond)
 
 		as.Equal(cp.ConnNum(), 0)
 		d = cp.ConnNumIde(raddr.Network(), raddr.String())
@@ -309,7 +287,7 @@ func Test_ConnPool_8(t *testing.T) {
 	})
 }
 
-//已经关闭的连接禁止加入
+// 已经关闭的连接禁止加入
 func Test_pools_1(t *testing.T) {
 	as := assert.New(t, true)
 
@@ -322,9 +300,9 @@ func Test_pools_1(t *testing.T) {
 			cp: &ConnPool{
 				IdeConn: 10,
 			},
-			occupy:  make(map[net.Conn]int),  //占据位置
-			vacancy: make(map[int]struct{}),  //空缺位置
-			conns:   make([]*connMan, 0, 10), //存在
+			occupy:  make(map[net.Conn]int),  // 占据位置
+			vacancy: make(map[int]struct{}),  // 空缺位置
+			conns:   make([]*connMan, 0, 10), // 存在
 		}
 
 		conn.Close()
@@ -334,11 +312,10 @@ func Test_pools_1(t *testing.T) {
 
 		conn1, err := ps.get()
 		as.ErrorIs(err, errorConnNotAvailable).Nil(conn1)
-
 	})
 }
 
-//连接加入池之后，关闭连接
+// 连接加入池之后，关闭连接
 func Test_pools_2(t *testing.T) {
 	as := assert.New(t, true)
 
@@ -351,9 +328,9 @@ func Test_pools_2(t *testing.T) {
 			cp: &ConnPool{
 				IdeConn: 10,
 			},
-			occupy:  make(map[net.Conn]int),  //占据位置
-			vacancy: make(map[int]struct{}),  //空缺位置
-			conns:   make([]*connMan, 0, 10), //存在
+			occupy:  make(map[net.Conn]int),  // 占据位置
+			vacancy: make(map[int]struct{}),  // 空缺位置
+			conns:   make([]*connMan, 0, 10), // 存在
 		}
 
 		ps.put(conn, 5*time.Second)
@@ -365,6 +342,5 @@ func Test_pools_2(t *testing.T) {
 
 		conn1, err := ps.get()
 		as.ErrorIs(err, errorConnNotAvailable).Nil(conn1)
-
 	})
 }
